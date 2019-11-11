52b396345fb982c9c31c305df269a794161948e4
John Lindquist <johnlindquist@gmail.com>
2019-11-11 16:45:01 -0700

    just started

---
+++ src/inject/inject.js
@@ -0,0 +1,16 @@
+// console.log(document.location)
+
+// if (document.location.href.includes("google")) {
+//   console.log("You are on GOOGLE!")
+//   const styleEl = document.createElement("style")
+//   styleEl.appendChild(
+//     document.createTextNode(
+//       `
+// 		img{
+// 			display: none;
+// 		}
+// 		`
+//     )
+//   )
+//   document.head.appendChild(styleEl)
+// }
